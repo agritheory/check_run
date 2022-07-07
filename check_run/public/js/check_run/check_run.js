eabf8a55e9d2bd01fffdfbc6a24e41f8391a3981
John Kelley <johnhkelley1@gmail.com>
2022-07-07 13:46:58 -0600

    fix: checkrun rerender on document change

--- check_run/public/js/check_run/check_run.js
+++ check_run/public/js/check_run/check_run.js
@@ -3,33 +3,30 @@ import CheckRun from './CheckRun.vue'
 frappe.provide('check_run')
 
 check_run.mount_table = frm => {
-	if (frm.$check_run instanceof Vue) {
-		console.log('existing vue app')
-		frm.check_run_state.docstatus = frm.doc.docstatus
-		frm.check_run_state.transactions = frm.transactions
-		frm.check_run_state.modes_of_payment = frm.check_run_state.modes_of_payment
-		return
-	}
-	const state = Vue.observable({
+	frm.check_run_state = Vue.observable({
 		transactions: frm.transactions,
 		party_filter: "",
 		docstatus: frm.doc.docstatus,
 		modes_of_payment: frm.modes_of_payment,
 		show_party_filter: false
 	})
-
-	frm.check_run_state = state
-
+	if (frm.$check_run instanceof Vue) {
+		frm.$check_run.$destroy();
+	}
+	$('#check-run-vue').remove()
+	$(frm.fields_dict['check_run_table'].wrapper).html($("<div id='check-run-vue'></div>").get(0));
 	frm.$check_run = new window.Vue({
-		el: $(frm.fields_dict['check_run_table'].wrapper).get(0),
+		el: $("#check-run-vue").get(0),
 		render: h => h(
 			CheckRun,
 			{ props: {
-				transactions: state.transactions, //list of transtactions
-				modes_of_payment: state.modes_of_payment, // populate modes_of_payment select. doesn't get updated
-				docstatus: state.docstatus, // used to conditionally render column inputs based on submission status. doesn't get updated
-				state: state
+				transactions: frm.check_run_state.transactions, //list of transtactions
+				modes_of_payment: frm.check_run_state.modes_of_payment, // populate modes_of_payment select. doesn't get updated
+				docstatus: frm.check_run_state.docstatus, // used to conditionally render column inputs based on submission status. doesn't get updated
+				state: frm.check_run_state
 			}
 		})
 	})
+
+
 }
