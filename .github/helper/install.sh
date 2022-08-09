84c2503eb4de613a44861d2066eaff0ad902dcf2
Robert Duncan <robirtduncan@gmail.com>
2022-08-09 19:19:34 +0000

    test: revert mariadb wildcard and install-app

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -14,9 +14,9 @@ cp -r "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
 
-mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'%' IDENTIFIED BY 'test_frappe'"
+mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE USER 'test_frappe'@'localhost' IDENTIFIED BY 'test_frappe'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE DATABASE test_frappe"
-mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'%'"
+mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_frappe\`.* TO 'test_frappe'@'localhost'"
 
 mysql --host 127.0.0.1 --port 3306 -u root -e "UPDATE mysql.user SET Password=PASSWORD('travis') WHERE User='root'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
@@ -35,10 +35,8 @@ cd ./apps/frappe || exit
 yarn add node-sass@4.13.1
 cd ../..
 
-bench get-app erpnext --branch version-13 --skip-assets
-bench get-app check_run "${GITHUB_WORKSPACE}" --skip-assets
-
-bench --site test_site install-app check_run erpnext
+bench get-app erpnext --branch version-13
+bench get-app check_run "${GITHUB_WORKSPACE}"
 
 bench start &> bench_run_logs.txt &
 bench --site test_site reinstall --yes
