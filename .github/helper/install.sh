cf7e3ec07c8f72396435ed800a18715e4e68898d
Robert Duncan <robirtduncan@gmail.com>
2022-08-09 16:19:28 +0000

    test: install apps

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -38,6 +38,8 @@ cd ../..
 bench get-app erpnext --branch version-13
 bench get-app check_run "${GITHUB_WORKSPACE}"
 
+bench install-app check_run erpnext
+
 bench start &> bench_run_logs.txt &
 bench --site test_site reinstall --yes
 CI=Yes bench build
