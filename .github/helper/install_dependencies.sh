350bb6e5648ed63c7086a9bd89f5acb194a5c85f
Tyler Matteson <tyler@agritheory.com>
2022-08-17 10:55:12 -0400

    CI (#8)
    * wip: json and py validate, semantic, and frappe json diff
    
    * test: stub UI test yaml - copied from Frappe
    
    * test: add helper shell files, remove job contitionals
    
    * test: remove producer/consumer test dbs from install script
    
    * test: correct install file / install dependency file
    
    * test: echo helper folder to debug path error
    
    * test: move helper folder out of workflows
    
    * test: remove echo
    
    * test: install erpnext
    
    * test: create site in sintall script
    
    * test: skip assets on erpnext install
    
    * test: remove frappe-path argument from bench init
    
    * wip: update JSON ci
    
    * test: remove python setup, already in ubuntu
    
    * test: frappe-bench needs 3.10
    
    * test: python3.9
    
    * test: no mariadb password
    
    * test: blank password
    
    * test: mysql password 123
    
    * test: using frappe/erpnext workflow
    
    * test: allow empty password = yes
    
    * test: file wasnt saved
    
    * test: mariadb version 10.5 => 10.3
    
    * test: various
    
    * test: reinsert pip install
    
    * test: fix typo
    
    * test: remove space (typo)
    
    * test: fix syntax
    
    * test: remove site setup
    
    * test: restore site without building frappe
    
    * test: remove db setup, only ui test
    
    * test: remove strawberry output for site setup
    
    * test: use check run test
    
    * test: cypress test data path
    
    * test: install apps on site
    
    * test: cypress path
    
    * test: bench restart after install, set developer mode
    
    * test: CI=Yes bench command
    
    * test: install apps
    
    * test: add site adn skip assets
    
    * test: ci=yes install-app
    
    * test: wildcard frappe user, remove ci bench install-app
    
    * test: update localhost wildcard both commands
    
    * text: bench execute command
    
    * test: revert mariadb wildcard and install-app
    
    * test: remove bench restart
    
    * test: remove CI=yes execute
    
    * wip: cypress testing fixup
    
    * wip: ach workflows
    
    * wip: ach generation
    
    * wip: check run ui tests V1
    
    * test: fix cypress command path
    
    * test: cypress command
    
    * test: cypress response timeout
    
    * test: remove headless
    
    * test: cypress action
    
    * test: rerun
    
    * test: "8000" + "/"
    
    * test: use cypress config
    
    * test: add bench restart
    
    * test: update baseUrl to match site_config.json
    
    * test: no recordings
    
    * test: video, screenshots off
    
    * wip: remove unused env vars, set script clone depth, remove commented yarn run, fix spec.js typo
    
    Co-authored-by: Robert Duncan <robirtduncan@gmail.com>

---
+++ .github/helper/install_dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check for merge conflicts before proceeding
+python -m compileall -f "${GITHUB_WORKSPACE}"
+if grep -lr --exclude-dir=node_modules "^<<<<<<< " "${GITHUB_WORKSPACE}"
+    then echo "Found merge conflicts"
+    exit 1
+fi
+
+sudo apt update && sudo apt install redis-server libcups2-dev
+
+ # install wkhtmltopdf
+wget -O /tmp/wkhtmltox.tar.xz https://github.com/frappe/wkhtmltopdf/raw/master/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+tar -xf /tmp/wkhtmltox.tar.xz -C /tmp
+sudo mv /tmp/wkhtmltox/bin/wkhtmltopdf /usr/local/bin/wkhtmltopdf
+sudo chmod o+x /usr/local/bin/wkhtmltopdf
