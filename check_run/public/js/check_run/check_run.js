dbfcc6de4d8820330039b822f89e9b11e4f07c77
Tyler Matteson <tyler@agritheory.com>
2023-02-28 16:38:13 -0500

    chore: prettier formatting

--- check_run/public/js/check_run/check_run.js
+++ check_run/public/js/check_run/check_run.js
@@ -11,51 +11,52 @@ check_run.mount_table = frm => {
 	})
 	frm.check_run_state = Vue.observable({
 		transactions: frm.transactions,
-		party_filter: "",
+		party_filter: '',
 		docstatus: frm.doc.docstatus,
 		modes_of_payment: frm.modes_of_payment,
 		show_party_filter: false,
-		check_run_total: function() {
+		check_run_total: function () {
 			return this.transactions.reduce((partialSum, t) => {
 				return t.pay ? partialSum + t.amount : partialSum
-			}, 0);
+			}, 0)
 		},
 		selectedRow: 0,
-		mopsOpen: 0
+		mopsOpen: 0,
 	})
 	if (frm.$check_run instanceof Vue) {
 		frm.$check_run.$destroy()
 	}
 	$('#check-run-vue').remove()
-	$(frm.fields_dict['check_run_table'].wrapper).html($("<div id='check-run-vue'></div>").get(0));
+	$(frm.fields_dict['check_run_table'].wrapper).html($("<div id='check-run-vue'></div>").get(0))
 	frm.$check_run = new window.Vue({
-		el: $("#check-run-vue").get(0),
-		render: h => h(
-			CheckRun,
-			{ props: {
-				transactions: frm.check_run_state.transactions, //list of transtactions
-				modes_of_payment: frm.check_run_state.modes_of_payment, // populate modes_of_payment select. doesn't get updated
-				docstatus: frm.check_run_state.docstatus, // used to conditionally render column inputs based on submission status. doesn't get updated
-				state: frm.check_run_state
-			}
-		})
+		el: $('#check-run-vue').get(0),
+		render: h =>
+			h(CheckRun, {
+				props: {
+					transactions: frm.check_run_state.transactions, //list of transtactions
+					modes_of_payment: frm.check_run_state.modes_of_payment, // populate modes_of_payment select. doesn't get updated
+					docstatus: frm.check_run_state.docstatus, // used to conditionally render column inputs based on submission status. doesn't get updated
+					state: frm.check_run_state,
+				},
+			}),
 	})
-
 }
 
 check_run.keyDownHandler = e => {
-
-	if(!check_run.frm) {
+	if (!check_run.frm) {
 		return
 	}
 
-	if(document.activeElement.tagName == "INPUT" || document.activeElement.tagName == "TEXTAREA") {
+	if (document.activeElement.tagName == 'INPUT' || document.activeElement.tagName == 'TEXTAREA') {
 		return
 	}
-	
-	if(e.keyCode == 40 && check_run.frm.check_run_state.selectedRow < (check_run.frm.check_run_state.transactions.length - 1)){
-		for(let j=0;j<check_run.frm.check_run_state.transactions.length;j++) {
-			if(check_run.frm.check_run_state.transactions[j].mopIsOpen) {
+
+	if (
+		e.keyCode == 40 &&
+		check_run.frm.check_run_state.selectedRow < check_run.frm.check_run_state.transactions.length - 1
+	) {
+		for (let j = 0; j < check_run.frm.check_run_state.transactions.length; j++) {
+			if (check_run.frm.check_run_state.transactions[j].mopIsOpen) {
 				return
 			}
 		}
@@ -63,9 +64,9 @@ check_run.keyDownHandler = e => {
 		check_run.frm.check_run_state.selectedRow += 1
 	}
 
-	if(e.keyCode == 38 && check_run.frm.check_run_state.selectedRow > 0){
-		for(let j=0;j<check_run.frm.check_run_state.transactions.length;j++) {
-			if(check_run.frm.check_run_state.transactions[j].mopIsOpen) {
+	if (e.keyCode == 38 && check_run.frm.check_run_state.selectedRow > 0) {
+		for (let j = 0; j < check_run.frm.check_run_state.transactions.length; j++) {
+			if (check_run.frm.check_run_state.transactions[j].mopIsOpen) {
 				return
 			}
 		}
@@ -73,17 +74,26 @@ check_run.keyDownHandler = e => {
 		check_run.frm.check_run_state.selectedRow -= 1
 	}
 
-	if(e.keyCode == 32 && check_run.frm.check_run_state.selectedRow != null && check_run.frm.check_run_state.transactions.length){
+	if (
+		e.keyCode == 32 &&
+		check_run.frm.check_run_state.selectedRow != null &&
+		check_run.frm.check_run_state.transactions.length
+	) {
 		e.preventDefault()
-		if(check_run.frm.check_run_component) {
+		if (check_run.frm.check_run_component) {
 			check_run.frm.check_run_component.checkPay()
 		}
 	}
 
-	if(e.keyCode && e.keyCode >= 65 && e.keyCode <= 90 && check_run.frm.check_run_state.selectedRow != null && check_run.frm.check_run_state.transactions.length){
+	if (
+		e.keyCode &&
+		e.keyCode >= 65 &&
+		e.keyCode <= 90 &&
+		check_run.frm.check_run_state.selectedRow != null &&
+		check_run.frm.check_run_state.transactions.length
+	) {
 		check_run.frm.check_run_component.openMopWithSearch(e.keyCode)
 	}
-
 }
 
 window.removeEventListener('keydown', check_run.keyDownHandler)
--- check_run/public/js/check_run/check_run_quick_entry.js
+++ check_run/public/js/check_run/check_run_quick_entry.js
@@ -1,4 +1,4 @@
-frappe.provide('frappe.ui.form');
+frappe.provide('frappe.ui.form')
 
 frappe.ui.form.CheckRunQuickEntryForm = class CheckRunQuickEntryForm extends frappe.ui.form.QuickEntryForm {
 	constructor(doctype, after_insert, init_callback, doc, force) {
@@ -6,24 +6,24 @@ frappe.ui.form.CheckRunQuickEntryForm = class CheckRunQuickEntryForm extends fra
 	}
 	render_dialog() {
 		this.mandatory = this.get_fields()
-		super.render_dialog();
+		super.render_dialog()
 		this.dialog.$wrapper.find('.btn-secondary').hide()
-		this.dialog.fields_dict["bank_account"].get_query = () => {
+		this.dialog.fields_dict['bank_account'].get_query = () => {
 			return {
-				"filters": {
-					"company": this.dialog.get_field("company").value,
-				}
+				filters: {
+					company: this.dialog.get_field('company').value,
+				},
 			}
 		}
-		this.dialog.fields_dict["pay_to_account"].get_query = () => {
+		this.dialog.fields_dict['pay_to_account'].get_query = () => {
 			return {
-				"filters": {
-					"company": this.dialog.get_field("company").value,
-					'account_type': 'Payable'
-				}
+				filters: {
+					company: this.dialog.get_field('company').value,
+					account_type: 'Payable',
+				},
 			}
 		}
-		this.dialog.fields_dict["company"].df.onchange = () => {
+		this.dialog.fields_dict['company'].df.onchange = () => {
 			this.default_accounts()
 		}
 		this.default_accounts()
@@ -31,42 +31,42 @@ frappe.ui.form.CheckRunQuickEntryForm = class CheckRunQuickEntryForm extends fra
 	get_fields() {
 		return [
 			{
-				label: __("Company"),
-				fieldname: "company",
-				fieldtype: "Link",
-				options: "Company",
+				label: __('Company'),
+				fieldname: 'company',
+				fieldtype: 'Link',
+				options: 'Company',
 				reqd: 1,
 			},
 			{
-				label: __("Paid From (Bank Account"),
-				fieldname: "bank_account",
-				fieldtype: "Link",
-				options: "Bank Account",
-				reqd: 1
+				label: __('Paid From (Bank Account'),
+				fieldname: 'bank_account',
+				fieldtype: 'Link',
+				options: 'Bank Account',
+				reqd: 1,
 			},
 			{
-				label: __("Payable Account"),
-				fieldname: "pay_to_account",
-				fieldtype: "Link",
-				options: "Account",
-				reqd: 1
-			}
+				label: __('Payable Account'),
+				fieldname: 'pay_to_account',
+				fieldtype: 'Link',
+				options: 'Account',
+				reqd: 1,
+			},
 		]
 	}
 	default_accounts() {
 		if (frappe.get_route() && frappe.get_route()[0] == 'Form') {
-			this.dialog.fields_dict["company"] = cur_frm.doc.company
-			this.dialog.fields_dict["pay_to_account"].set_value(cur_frm.doc.pay_to_account)
-			this.dialog.fields_dict["bank_account"].set_value(cur_frm.doc.bank_account)
+			this.dialog.fields_dict['company'] = cur_frm.doc.company
+			this.dialog.fields_dict['pay_to_account'].set_value(cur_frm.doc.pay_to_account)
+			this.dialog.fields_dict['bank_account'].set_value(cur_frm.doc.bank_account)
 		} else {
 			let company = this.dialog.fields_dict.company.get_value()
-			frappe.db.get_value('Company', company, 'default_payable_account')
+			frappe.db.get_value('Company', company, 'default_payable_account').then(r => {
+				this.dialog.fields_dict['pay_to_account'].set_value(r.message.default_payable_account)
+			})
+			frappe.db
+				.get_value('Bank Account', { company: company, is_default: 1, is_company_account: 1 }, 'name')
 				.then(r => {
-					this.dialog.fields_dict["pay_to_account"].set_value(r.message.default_payable_account)
-				})
-			frappe.db.get_value('Bank Account', { company: company, is_default: 1, is_company_account: 1 }, 'name')
-				.then(r => {
-					this.dialog.fields_dict["bank_account"].set_value(r.message.name)
+					this.dialog.fields_dict['bank_account'].set_value(r.message.name)
 				})
 		}
 	}
@@ -74,11 +74,15 @@ frappe.ui.form.CheckRunQuickEntryForm = class CheckRunQuickEntryForm extends fra
 		const me = this
 		this.dialog.set_primary_action(__('Start Check Run'), () => {
 			let values = me.dialog.get_values()
-			frappe.xcall("check_run.check_run.doctype.check_run.check_run.check_for_draft_check_run",
-				{ company: values.company, bank_account: values.bank_account, payable_account: values.pay_to_account }
-			).then(r => {
-				frappe.set_route("Form", "Check Run", r)
-			})
+			frappe
+				.xcall('check_run.check_run.doctype.check_run.check_run.check_for_draft_check_run', {
+					company: values.company,
+					bank_account: values.bank_account,
+					payable_account: values.pay_to_account,
+				})
+				.then(r => {
+					frappe.set_route('Form', 'Check Run', r)
+				})
 		})
 	}
 }
