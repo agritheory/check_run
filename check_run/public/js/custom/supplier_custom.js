59c13011c840fd44e988c62d1ea130159d4c19ec
Tyler Matteson <tyler@agritheory.com>
2023-02-28 11:53:34 -0500

    fix: don't raise exception on bank account lookup

--- check_run/public/js/custom/supplier_custom.js
+++ check_run/public/js/custom/supplier_custom.js
@@ -18,27 +18,27 @@ function show_bank_account_number(frm) {
 	frappe.msgprint(msg, "Bank Information")
 } else {
 	frappe.xcall('check_run.check_run.show_bank_account_number', { doctype: frm.doc.doctype, docname: frm.doc.name })
-		.then(r => {
-			let msg = `Routing Number ${r.routing_number}<br>Account Number: ${r.account_number}`
-			frappe.msgprint(msg, "Bank Information")
-		})
+	.then(r => {
+		let msg = `Routing Number ${r.routing_number}<br>Account Number: ${r.account_number}`
+		frappe.msgprint(msg, "Bank Information")
+	})
 }
 }
 
 function set_required_banking_fields(frm) {
 	if (!frm.doc.supplier_default_mode_of_payment) { return }
 	frappe.db.get_value('Mode of Payment', frm.doc.supplier_default_mode_of_payment, 'type')
-		.then(r => {
-			if (r.message.type == 'Electronic') {
-				frm.set_df_property('bank', 'reqd', 1)
-				frm.set_df_property('bank', 'hidden', 0)
-				frm.set_df_property('bank_account', 'reqd', 1)
-				frm.set_df_property('bank_account', 'hidden', 0)
-			} else {
-				frm.set_df_property('bank', 'reqd', 0)
-				frm.set_df_property('bank', 'hidden', 1)
-				frm.set_df_property('bank_account', 'reqd', 0)
-				frm.set_df_property('bank_account', 'hidden', 1)
-			}
-		})
+	.then(r => {
+		if (r.message.type == 'Electronic') {
+			frm.set_df_property('bank', 'reqd', 1)
+			frm.set_df_property('bank', 'hidden', 0)
+			frm.set_df_property('bank_account', 'reqd', 1)
+			frm.set_df_property('bank_account', 'hidden', 0)
+		} else {
+			frm.set_df_property('bank', 'reqd', 0)
+			frm.set_df_property('bank', 'hidden', 1)
+			frm.set_df_property('bank_account', 'reqd', 0)
+			frm.set_df_property('bank_account', 'hidden', 1)
+		}
+	})
 }
