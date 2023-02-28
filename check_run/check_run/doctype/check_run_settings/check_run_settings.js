b283f93689f3a881432a44d84701484932e6372d
agritheory <agritheory@users.noreply.github.com>
2023-02-28 21:02:27 +0000

    style: prettify code

--- check_run/check_run/doctype/check_run_settings/check_run_settings.js
+++ check_run/check_run/doctype/check_run_settings/check_run_settings.js
@@ -1,8 +1,7 @@
 // Copyright (c) 2022, AgriTheory and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Check Run Settings', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on("Check Run Settings", {
+  // refresh: function(frm) {
+  // }
 });
