c4357a80305b4b5d2137b132553eb38b4bb6e2a7
Tyler Matteson <tyler@agritheory.com>
2023-02-28 11:57:11 -0500

    Bank account lookup fix (#61)
    * feat: fix lookup for non-existient bank account info, improve UX
    
    * fix: don't raise exception on bank account lookup

--- check_run/check_run/__init__.py
+++ check_run/check_run/__init__.py
@@ -4,6 +4,6 @@ import frappe
 @frappe.whitelist()
 def show_bank_account_number(doctype, docname):
 	doc = frappe.get_doc(doctype, docname)
-	routing_number = frappe.get_value('Bank', doc.bank, 'aba_number')
-	account_number = doc.get_password('bank_account')
+	routing_number = frappe.get_value('Bank', doc.bank, 'aba_number') or ''
+	account_number = doc.get_password('bank_account', raise_exception=False) or ''
 	return {'routing_number': routing_number, 'account_number': account_number}
