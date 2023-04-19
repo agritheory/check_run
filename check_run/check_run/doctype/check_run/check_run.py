466bdda43af34f0a5d697c100d36a21edfb5af1e
Trusted Computer <75872475+trustedcomputer@users.noreply.github.com>
2023-04-19 15:03:58 -0700

    Update individual_id_number
    Strip non-alphanumeric characters from individual_id_number and make it all upper case.

--- check_run/check_run/doctype/check_run/check_run.py
+++ check_run/check_run/doctype/check_run/check_run.py
@@ -553,9 +553,9 @@ def build_nacha_file_from_payment_entries(doc, payment_entries, settings):
 			if not party_bank_routing_number:
 				exceptions.append(f'{pe.party_type} Bank Routing Number missing for {pe.party_name}')
 		if settings.get('individual_id_number_from') == "Naming Series":
-			individual_id_number=pe.party
+			individual_id_number=''.join(c for c in pe.party if c.isalnum()).upper()
 		elif settings.get('individual_id_number_from') == "Party Name":
-			individual_id_number=pe.party_name
+			individual_id_number=''.join(c for c in pe.party_name if c.isalnum()).upper()
 		else:
 			individual_id_number=''
 		ach_entry = ACHEntry(
