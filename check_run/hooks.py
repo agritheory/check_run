45c959834a60d57148b4443e902f25c2c56a30ff
Tyler Matteson <tyler@agritheory.com>
2023-07-24 14:25:36 -0400

    feat: migrate validation to doc_events from doctype override (#127)
    * feat: migrate validation to doc_events from doctype override
    
    * feat: disallow cancellation of source documents selected for payment in draft CRs (#126)
    
    * feat: migrate validation to doc_events from doctype override
    
    * feat: disallow cancellation of source documents selected for payment in draft CRs (#126)

--- check_run/hooks.py
+++ check_run/hooks.py
@@ -93,23 +93,24 @@ after_migrate = "check_run.customize.load_customizations"
 # ---------------
 # Override standard doctype classes
 
-override_doctype_class = {"Bank": "check_run.overrides.bank.CustomBank"}
+# override_doctype_class = {"Bank": "check_run.overrides.bank.CustomBank"}
 
 # Document Events
 # ---------------
 # Hook on document methods and events
 
 doc_events = {
-	"Payment Entry": {
-		"on_submit": "check_run.overrides.payment_entry.update_check_number",
-	},
-	"Purchase Invoice": {
-		"before_cancel": ["check_run.check_run.disallow_cancellation_if_in_check_run"]
-	},
+	"Bank": {"validate": ["check_run.overrides.bank.validate"]},
 	"Expense Claim": {
 		"before_cancel": ["check_run.check_run.disallow_cancellation_if_in_check_run"]
 	},
 	"Journal Entry": {
+		"before_cancel": ["check_run.check_run.disallow_cancellation_if_in_check_run"]
+	},
+	"Payment Entry": {
+		"on_submit": "check_run.overrides.payment_entry.update_check_number",
+	},
+	"Purchase Invoice": {
 		"before_cancel": ["check_run.check_run.disallow_cancellation_if_in_check_run"]
 	},
 }
