30679db1985fcc0a41238d8860a17c019b751d98
Tyler Matteson <tyler@agritheory.com>
2023-08-15 11:44:36 -0400

    fix: only increment if check numer is numeric (#138)

--- check_run/overrides/payment_entry.py
+++ check_run/overrides/payment_entry.py
@@ -6,7 +6,11 @@ def update_check_number(doc, method=None):
 	mode_of_payment_type = frappe.db.get_value(
 		"Mode of Payment", doc.mode_of_payment, "type"
 	)
-	if doc.bank_account and mode_of_payment_type == "Bank":
+	if (
+		doc.bank_account
+		and mode_of_payment_type == "Bank"
+		and str(doc.reference_no).isdigit()
+	):
 		frappe.db.set_value(
 			"Bank Account", doc.bank_account, "check_number", doc.reference_no
 		)
