1dd2bd1e41179f02ecb222cd0fa6041b2a37267c
Tyler Matteson <tyler@agritheory.com>
2023-09-07 13:55:56 -0400

    chore: more typing

--- check_run/overrides/payment_entry.py
+++ check_run/overrides/payment_entry.py
@@ -6,7 +6,7 @@ from erpnext.accounts.doctype.payment_entry.payment_entry import PaymentEntry
 
 
 @frappe.whitelist()
-def update_check_number(doc: PaymentEntry, method: str | None = None):
+def update_check_number(doc: PaymentEntry, method: str | None = None) -> None:
 	mode_of_payment_type = frappe.db.get_value("Mode of Payment", doc.mode_of_payment, "type")
 	if doc.bank_account and mode_of_payment_type == "Bank" and str(doc.reference_no).isdigit():
 		frappe.db.set_value("Bank Account", doc.bank_account, "check_number", doc.reference_no)
