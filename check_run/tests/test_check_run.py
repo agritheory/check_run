c141fdfe1a6282fe72f0a777dc6491b7c9c9953c
Tyler Matteson <tyler@agritheory.com>
2023-10-23 15:03:20 -0400

    wip: check run tests

--- check_run/tests/test_check_run.py
+++ check_run/tests/test_check_run.py
@@ -11,6 +11,8 @@ year = datetime.date.today().year
 
 @pytest.fixture
 def cr():  # return draft check run
+	if frappe.db.exists("Check Run", f"ACC-CR-{year}-00001"):
+		return frappe.get_doc("Check Run", f"ACC-CR-{year}-00001")
 	cr = frappe.new_doc("Check Run")
 	cr.company = "Chelsea Fruit Co"
 	cr.bank_account = "Primary Checking - Local Bank"
@@ -19,20 +21,43 @@ def cr():  # return draft check run
 	cr.set_last_check_number()
 	cr.set_default_payable_account()
 	entries = get_entries(cr)
-	# manipulate entries / selections
+	for row in entries.get("transactions"):
+		row["pay"] = False
+	cr.transactions = frappe.as_json(entries.get("transactions"))
+	cr.flags.in_test = True
 	cr.save()
-	cr.entries = frappe.as_json(entries.get("transactions"))
 	return cr
 
 
 def test_get_entries(cr):
 	crs = get_check_run_settings(cr)
 	assert frappe.db.exists("Check Run Settings", crs)
-	cr.entries = frappe.utils.safe_json_loads(cr.entries)
-	assert len(cr.entries) > 1
+	cr.transactions = frappe.utils.safe_json_loads(cr.transactions)
+	assert len(cr.transactions) > 1
 	# assert that each type of source document appears at least once
-	assert any([doc.get("doctype") == "Purchase Invoice" for doc in cr.entries])
-	assert any([doc.get("doctype") == "Journal Entry" for doc in cr.entries])
-	assert any([doc.get("doctype") == "Expense Claim" for doc in cr.entries])
+	assert any([doc.get("doctype") == "Purchase Invoice" for doc in cr.transactions])
+	assert any([doc.get("doctype") == "Journal Entry" for doc in cr.transactions])
+	assert any([doc.get("doctype") == "Expense Claim" for doc in cr.transactions])
 	# assert that the invoice with installment payment schedule appears more than once
-	assert len([doc.get("name") == f"ACC-PINV-{year}-00001 " for doc in cr.entries]) > 1
+	assert len([doc.get("name") == f"ACC-PINV-{year}-00001 " for doc in cr.transactions]) > 1
+
+
+def test_process_check_run_on_hold_invoice(cr):
+	cr.transactions = frappe.utils.safe_json_loads(cr.transactions)
+	# try to pay invoice on hold to raise error
+	for row in cr.transactions:
+		if row.get("party") == "Liu & Loewen Accountants LLP":
+			row["pay"] = True
+	cr.transactions = frappe.as_json(cr.transactions)
+	cr.flags.in_test = True
+	cr.save()
+	with pytest.raises(
+		frappe.exceptions.ValidationError, match=f"Purchase Invoice ACC-PINV-{year}-00020 is on hold"
+	):
+		# cr.flags.in_test = True
+		cr.process_check_run()
+	crs = get_check_run_settings(cr)
+	crs.automatically_release_on_hold_invoices = True
+	crs.save()
+	cr.flags.in_test = True
+	cr.process_check_run()
