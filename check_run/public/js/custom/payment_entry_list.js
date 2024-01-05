447a3894758b4c6a28a30bd493ec3c8f4da8c4ae
fproldan <fproldan@users.noreply.github.com>
2024-01-05 13:00:29 +0000

    style: prettify code

--- check_run/public/js/custom/payment_entry_list.js
+++ check_run/public/js/custom/payment_entry_list.js
@@ -1,17 +1,20 @@
-
 frappe.listview_settings['Payment Entry'] = {
-    has_indicator_for_draft: 1,
+	has_indicator_for_draft: 1,
 	has_indicator_for_cancelled: 1,
 	get_indicator: function (doc) {
 		if (doc.status) {
-			return [__(doc.status), {
-				"Draft": "orange",
-				"Submitted": "green",
-				"Cancelled": "gray",
-				"Voided": "purple"
-			}[doc.status], "status,=," + doc.status]
+			return [
+				__(doc.status),
+				{
+					Draft: 'orange',
+					Submitted: 'green',
+					Cancelled: 'gray',
+					Voided: 'purple',
+				}[doc.status],
+				'status,=,' + doc.status,
+			]
 		} else {
-			return [__("Not Set"), "blue", "status,=,'Not Set'"]
+			return [__('Not Set'), 'blue', "status,=,'Not Set'"]
 		}
-	}
+	},
 }
