d0a74376597fe1a66a6495f8f93db2441f142a7a
Tyler Matteson <tyler@agritheory.com>
2024-01-17 20:27:59 -0500

    fix: remove validation

--- check_run/overrides/payment_entry.py
+++ check_run/overrides/payment_entry.py
@@ -5,7 +5,7 @@ import frappe
 from frappe.utils import get_link_to_form
 from erpnext.accounts.doctype.payment_entry.payment_entry import PaymentEntry
 from erpnext.accounts.general_ledger import make_gl_entries, process_gl_map
-from frappe.utils.data import getdate, flt
+from frappe.utils.data import getdate
 
 
 class CustomPaymentEntry(PaymentEntry):
@@ -58,32 +58,6 @@ def update_check_number(doc: PaymentEntry, method: str | None = None) -> None:
 	mode_of_payment_type = frappe.db.get_value("Mode of Payment", doc.mode_of_payment, "type")
 	if doc.bank_account and mode_of_payment_type == "Bank" and str(doc.reference_no).isdigit():
 		frappe.db.set_value("Bank Account", doc.bank_account, "check_number", doc.reference_no)
-
-
-@frappe.whitelist()
-def validate_outstanding_payment_terms(doc: PaymentEntry, method: str | None = None) -> None:
-	references = doc.get("references")
-	errors = []
-	for r in references:
-		if r.reference_doctype != "Purchase Invoice":
-			continue
-		pmt_terms = frappe.get_all(
-			"Payment Schedule",
-			[
-				["parent", "=", r.reference_name],
-				["outstanding", "NOT IN", [0.0, flt(r.allocated_amount)]],
-			],
-			debug=True,
-		)
-		if doc.check_run and pmt_terms and not r.payment_term:
-			errors.append((r.idx, r.reference_name))
-
-	if errors:
-		list_items = "</li><li>".join([f"Row {row}: {inv}" for row, inv in errors])
-		message = frappe._(
-			f"There is at least one outstanding Payment Schedule Payment Term for the following Purchase Invoices in the Payment References table:<br><br><ul><li>{list_items}</li></ul><br>Please update the Payment Term field to tie this Payment Entry to the invoice's Payment Schedule and prevent paid invoice portions from showing up in a Check Run."
-		)
-		frappe.throw(msg=message, title=frappe._("Partially Paid Invoices"))
 
 
 @frappe.whitelist()
