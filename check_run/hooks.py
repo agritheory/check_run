efda8ae0f58ce272eae7888a0dd867a7ede4c792
Tyler Matteson <support@agritheory.dev>
2024-01-17 20:28:36 -0500

    fix: remove validation (#194)

--- check_run/hooks.py
+++ check_run/hooks.py
@@ -109,7 +109,6 @@ doc_events = {
 	"Bank": {"validate": ["check_run.overrides.bank.validate"]},
 	"Payment Entry": {
 		"validate": [
-			"check_run.overrides.payment_entry.validate_outstanding_payment_terms",
 			"check_run.overrides.payment_entry.validate_duplicate_check_number",
 		],
 		"on_submit": ["check_run.overrides.payment_entry.update_check_number"],
