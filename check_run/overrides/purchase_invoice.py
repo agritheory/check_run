5ca432586da840d324e56c91c6324829db3b30bb
viral <viral@fosserp.com>
2024-04-16 07:58:55 +0000

    custom field Allow stand-alone debit notes?

--- check_run/overrides/purchase_invoice.py
+++
@@ -1,6 +0,0 @@
-import frappe
-
-
-@frappe.whitelist()
-def get_buying_settings():
-	return frappe.get_doc("Buying Settings")
--- check_run/public/js/custom/purchase_invoice_custom.js
+++ check_run/public/js/custom/purchase_invoice_custom.js
@@ -1,13 +1,18 @@
 frappe.ui.form.on('Purchase Invoice', {
 	refresh: frm => {
-		frappe.call({
-			method: 'check_run.overrides.purchase_invoice.get_buying_settings',
-			args: {},
-			callback: r => {
-				if (r.message.allow_stand_alone_debit_notes == 'NO') {
-					frm.set_df_property('is_return', 'hidden', 1)
-				}
-			},
+		frm.trigger('stop_stand_alon_returns')
+	},
+	company: frm => {
+		frm.trigger('stop_stand_alon_returns')
+	},
+	stop_stand_alon_returns: frm => {
+		frappe.model.get_value('Company', frm.doc.company, 'allow_stand_alone_debit_notes', r => {
+			if (r.allow_stand_alone_debit_notes == 'No') {
+				frm.set_df_property('is_return', 'hidden', 1)
+			}
+			if (r.allow_stand_alone_debit_notes == 'Yes') {
+				frm.set_df_property('is_return', 'hidden', 0)
+			}
 		})
 	},
 })
