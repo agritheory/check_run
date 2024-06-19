624f36ebf195e734795216cdc790b0b1112ee802
Tyler Matteson <support@agritheory.dev>
2024-06-19 15:49:25 -0400

    Fix payment schedule outstanding v15 (#254)
    * fix: fix payment schedule outstanding
    
    * test: assert parent doc amount matches
    
    * tests: fix matching error message
    
    * feat: on_cancel hook and tests
    
    * feat: minor changes, add precision (#253)
    
    * fix: isort, JE generation issue
    
    ---------
    
    Co-authored-by: Francisco Roldán <franciscoproldan@gmail.com>

--- check_run/hooks.py
+++ check_run/hooks.py
@@ -110,9 +110,14 @@ doc_events = {
 	"Payment Entry": {
 		"validate": [
 			"check_run.overrides.payment_entry.validate_duplicate_check_number",
-			"check_run.overrides.payment_entry.validate_add_payment_term",
 		],
-		"on_submit": ["check_run.overrides.payment_entry.update_check_number"],
+		"on_submit": [
+			"check_run.overrides.payment_entry.update_outstanding_amount",
+			"check_run.overrides.payment_entry.update_check_number",
+		],
+		"on_cancel": [
+			"check_run.overrides.payment_entry.update_outstanding_amount",
+		],
 	},
 	"Purchase Invoice": {
 		"before_cancel": ["check_run.check_run.disallow_cancellation_if_in_check_run"]
