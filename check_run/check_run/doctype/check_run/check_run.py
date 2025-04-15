2b1d10441567fd307b41946243ecc1d0cd2991bf
Tyler Matteson <tyler@agritheory.com>
2025-04-15 11:10:40 -0400

    feat: add permission check on process check run API

--- check_run/check_run/doctype/check_run/check_run.py
+++ check_run/check_run/doctype/check_run/check_run.py
@@ -915,6 +915,9 @@ def ach_only(docname: str) -> dict:
 
 @frappe.whitelist()
 def process_check_run(docname: str) -> None:
+	has_permission(
+		"Check Run", ptype="submit", verbose=False, user=frappe.session.user, raise_exception=True
+	)
 	doc = frappe.get_doc("Check Run", docname)
 	doc.process_check_run()
 
