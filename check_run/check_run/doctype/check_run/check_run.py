441350eeef8af223a4406ff392726088f0554a41
Tyler Matteson <tyler@agritheory.com>
2025-05-20 13:33:01 -0400

    feat: backport anticipate_timestamp_mismatch_error

--- check_run/check_run/doctype/check_run/check_run.py
+++ check_run/check_run/doctype/check_run/check_run.py
@@ -545,6 +545,8 @@ def get_entries(doc: CheckRun | str) -> dict:
 		settings = None
 	if frappe.db.exists("Check Run", doc.name):
 		db_doc = frappe.get_doc("Check Run", doc.name)
+		if get_datetime(doc.modified) < db_doc.modified:
+			doc.reload()
 		if doc.end_date == db_doc.end_date and db_doc.transactions:
 			if db_doc.docstatus == 0:
 				outstanding_transaction = []
