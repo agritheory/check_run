0849331a96a7cfc2dd7bf48a27f21fd3717161b1
Tyler Matteson <tyler@agritheory.com>
2026-07-02 15:19:51 -0400

    chore: migrate to version-16
    Updated install.sh for version-16 and Postgres CI

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -4,6 +4,8 @@ export PIP_ROOT_USER_ACTION=ignore
 
 set -e
 
+DB="${DB:-mariadb}"
+
 # Check for merge conflicts before proceeding
 python -m compileall -f "${GITHUB_WORKSPACE}"
 if grep -lr --exclude-dir=node_modules "^<<<<<<< " "${GITHUB_WORKSPACE}"
@@ -16,28 +18,31 @@ cd ~ || exit
 pip install --upgrade pip
 pip install frappe-bench
 
-mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'"
-mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
-
-mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE OR REPLACE DATABASE test_site"
-mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE OR REPLACE USER 'test_site'@'localhost' IDENTIFIED BY 'test_site'"
-mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_site\`.* TO 'test_site'@'localhost'"
-
-mysql --host 127.0.0.1 --port 3306 -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'"  # match site_cofig
-mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
-
-if [ "${GITHUB_EVENT_NAME}" = 'pull_request' ]; then
-    BRANCH_NAME="${GITHUB_BASE_REF}"
-else
-    BRANCH_NAME="${GITHUB_REF_NAME}"
+if [ "$DB" == "mariadb" ]; then
+  mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'"
+  mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"
+  mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE OR REPLACE DATABASE test_site"
+  mysql --host 127.0.0.1 --port 3306 -u root -e "CREATE OR REPLACE USER 'test_site'@'localhost' IDENTIFIED BY 'test_site'"
+  mysql --host 127.0.0.1 --port 3306 -u root -e "GRANT ALL PRIVILEGES ON \`test_site\`.* TO 'test_site'@'localhost'"
+  mysql --host 127.0.0.1 --port 3306 -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root'"  # match site_cofig
+  mysql --host 127.0.0.1 --port 3306 -u root -e "FLUSH PRIVILEGES"
 fi
+
 echo "BRANCH_NAME: ${BRANCH_NAME}"
 
 git clone https://github.com/frappe/frappe --branch "${BRANCH_NAME}"
 bench init frappe-bench --frappe-path ~/frappe --python "$(which python)" --skip-assets --ignore-exist
 
 mkdir ~/frappe-bench/sites/test_site
-cp -r "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites/test_site/
+if [ "$DB" == "postgres" ]; then
+  cp "${GITHUB_WORKSPACE}/.github/helper/site_config_postgres.json" ~/frappe-bench/sites/test_site/site_config.json
+  echo "travis" | psql -h 127.0.0.1 -p 5432 -c "CREATE DATABASE test_site" -U postgres
+  echo "travis" | psql -h 127.0.0.1 -p 5432 -c "CREATE USER test_site WITH PASSWORD 'test_site'" -U postgres
+  echo "travis" | psql -h 127.0.0.1 -p 5432 -c "GRANT ALL PRIVILEGES ON DATABASE test_site TO test_site" -U postgres
+else
+  cp "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites/test_site/site_config.json
+fi
+
 
 cd ~/frappe-bench || exit
 
